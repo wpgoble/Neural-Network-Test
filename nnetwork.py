ee51a1a9f7d4c5273217715dc8c34e260b95e095
William Goble <wg13b@my.fsu.edu>
2019-08-21 08:11:20 -0400

    corrects error with random number generation
    Also, added comments so its easier to follow along

--- nnetwork.py
+++ nnetwork.py
@@ -1,27 +1,51 @@
-import numpy as np 
+import numpy as np
 
+# sigmoid function to normalize inputs
 def sigmoid(x):
-	return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
-training_inputs = np.array([[0, 0, 1],
-							[1, 1, 1],
-							[1, 0, 1],
-							[0, 1, 1]])
+# sigmoid derivatives to adjust synaptic weights
+def sigmoid_derivative(x):
+    return x * (1 - x)
 
-training_outputs = np.array([[0, 1, 1, 0]]).T
+# input dataset
+training_inputs = np.array([[0,0,1],
+                            [1,1,1],
+                            [1,0,1],
+                            [0,1,1]])
 
+# output dataset
+training_outputs = np.array([[0,1,1,0]]).T
+
+# seed random numbers to make calculation
 np.random.seed(1)
 
-synaptic_weights = 2 + np.random.random((3, 1)) - 1
+# initialize weights randomly with mean 0 to create weight matrix, synaptic weights
+synaptic_weights = 2 * np.random.random((3,1)) - 1
 
-print("Random starting synaptic weights: ")
+print('Random starting synaptic weights: ')
 print(synaptic_weights)
 
-for iteration in range(1):
+# Iterate 10,000 times
+for iteration in range(10000):
 
-	input_layer = training_inputs
+    # Define input layer
+    input_layer = training_inputs
+    # Normalize the product of the input layer with the synaptic weights
+    outputs = sigmoid(np.dot(input_layer, synaptic_weights))
 
-	outputs = sigmoid(np.dot(input_layer, synaptic_weights))
+    # how much did we miss?
+    error = training_outputs - outputs
 
-print("Outputs after training: ")
+    # multiply how much we missed by the
+    # slope of the sigmoid at the values in outputs
+    adjustments = error * sigmoid_derivative(outputs)
+
+    # update weights
+    synaptic_weights += np.dot(input_layer.T, adjustments)
+
+print('Synaptic weights after training: ')
+print(synaptic_weights)
+
+print("Output After Training:")
 print(outputs)
